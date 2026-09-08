a111d7740aa255c676ea65c51da8a01d1c6f0df8
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2026-09-08 17:51:36 +0200

    acronym_builder.py: daily coding challenges: 8 de septiembre 2026

---
+++ acronym_builder.py
@@ -0,0 +1,13 @@
+ignore_words = ["a", "for", "an", "and", "by", "of"]
+
+def build_acronym(s):
+    words = s.split()
+    s = ""
+
+    for word in words:
+        if word not in ignore_words:
+            s += word[0].upper()
+
+    return s
+
+print(build_acronym("For your information"))
